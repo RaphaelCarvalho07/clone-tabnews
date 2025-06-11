a79b72ff9dfb944c823c9f06052c5b345aded1f9
RaphaelCarvalho07 <raphaelcarvalho@Raphaels-MacBook-Pro.local>
2025-06-11 14:52:19 -0300

    add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-function Home () {
-    return <h1>Sabrina, eu te amo muito. Se você me ama, dá uma risadinha! 😎</h1>
+function Home() {
+  return (
+    <h1>Sabrina, eu te amo muito. Se você me ama, dá uma risadinha! 😎</h1>
+  );
 }
 
 export default Home;
